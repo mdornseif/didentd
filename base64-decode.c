a0d0457a2b609965814f4b1af3cb553b81618a76
drt <drt@2d70b15c-62de-0310-b45c-a2b26695ab02>
2000-04-20 15:05:07 +0000

    Changed for use with didentd
    git-svn-id: http://svn.23.nu/svn/repos/didentd/trunk@269 2d70b15c-62de-0310-b45c-a2b26695ab02

--- base64-decode.c
+++ base64-decode.c
@@ -1,76 +1,82 @@
-/* public domain */
+/* $Id
+ * 
+ * Found somewere on the internet stating:
+ */
 
+/* public domain */
 /* BASE64 on stdin -> converted data on stdout */
 
-#include <stdio.h>
-
-#ifdef _WIN32
-#ifndef WIN32
-#define WIN32
-#endif
-#endif
-
-#ifdef WIN32
-#include <io.h>
-#include <fcntl.h>
-#endif
+/* Hacked by drt@ailis.de to be a library function working on memory blocks
+ *
+ * $Log: base64-decode.c,v $
+ * Revision 1.2  2000/04/20 15:05:07  drt
+ * Changed for use with didentd
+ *
+ */ 
 
 unsigned char alphabet[64] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
-int
-main()
+int base64decode(char *dest, char *src, int l)
 {
     static char inalphabet[256], decoder[256];
     int i, bits, c, char_count, errors = 0;
+    int rpos;
+    int wpos = 0;
 
-#ifdef WIN32
-    _setmode( _fileno(stdout), _O_BINARY);
-#endif
-
-    for (i = (sizeof alphabet) - 1; i >= 0 ; i--) {
+    /* Build decodingtable */
+    for (i = (sizeof alphabet) - 1; i >= 0 ; i--) 
+      {
 	inalphabet[alphabet[i]] = 1;
 	decoder[alphabet[i]] = i;
-    }
+      }
 
     char_count = 0;
     bits = 0;
-    while ((c = getchar()) != EOF) {
+    for(rpos = 0; rpos < l; rpos++)
+      {
+	c = src[rpos];
+
 	if (c == '=')
-	  break;
+	  {
+	    break;
+	  }
+
 	if (c > 255 || ! inalphabet[c])
-	  continue;
+	  {
+	    /* woa shouldn't we barf here */
+	    continue;
+	  }
+
 	bits += decoder[c];
 	char_count++;
-	if (char_count == 4) {
-	    putchar((bits >> 16));
-	    putchar(((bits >> 8) & 0xff));
-	    putchar((bits & 0xff));
+	if (char_count < 4) 
+	  {
+	    bits <<= 6;
+	  }	 
+	else
+	  {
+	    dest[wpos++] = bits >> 16;
+	    dest[wpos++] = (bits >> 8) & 0xff;
+	    dest[wpos++] = bits & 0xff;
 	    bits = 0;
 	    char_count = 0;
-	} else {
-	    bits <<= 6;
-	}
-    }
-    if (c == EOF) {
-	if (char_count) {
-	    fprintf(stderr, "base64 encoding incomplete: at least %d bits truncated",
-		    ((4 - char_count) * 6));
-	    errors++;
-	}
-    } else { /* c == '=' */
-	switch (char_count) {
-	  case 1:
-	    fprintf(stderr, "base64 encoding incomplete: at least 2 bits missing");
-	    errors++;
-	    break;
-	  case 2:
-	    putchar((bits >> 10));
-	    break;
-	  case 3:
-	    putchar((bits >> 16));
-	    putchar(((bits >> 8) & 0xff));
-	    break;
-	}
-    }
-    exit(errors ? 1 : 0);
+	  }  
+      }
+      
+    /* c == '=' */
+    switch (char_count) 
+      {
+      case 1:
+	return -1;
+	break;
+      case 2:
+	dest[wpos++] = bits >> 10;
+	break;
+      case 3:
+	dest[wpos++] = bits >> 16;
+	dest[wpos++] = (bits >> 8) & 0xff;
+	break;
+      }
+    
+    return wpos;
 }
