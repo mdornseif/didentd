25fdddb11477c9d33353bc63a9fe49747d1b9daa
drt <drt@2d70b15c-62de-0310-b45c-a2b26695ab02>
2001-10-11 15:43:07 +0000

    misc fixes, mainly dokumentation
    git-svn-id: http://svn.23.nu/svn/repos/didentd/trunk@305 2d70b15c-62de-0310-b45c-a2b26695ab02

--- didentd-genanswer-crypt.c
+++ didentd-genanswer-crypt.c
@@ -1,4 +1,4 @@
-/* $Id: didentd-genanswer-crypt.c,v 1.9 2001/10/08 12:51:21 drt Exp $
+/* $Id: didentd-genanswer-crypt.c,v 1.10 2001/10/11 15:43:07 drt Exp $
  *  --drt@un.bewaff.net
  *
  * - generate a RfC 1413 reply containing 
@@ -6,38 +6,9 @@
  *   remoteip, remoteport and a timestamp
  *   in encrypted form
  * 
- * You might find more info at http://rc23.cx/
+ * You might find more info at http://c0re.jp/c0de/didentd/
  *
  * I do not belive there is something like copyright. 
- *
- * $Log: didentd-genanswer-crypt.c,v $
- * Revision 1.9  2001/10/08 12:51:21  drt
- * uodated emailaddress
- *
- * Revision 1.8  2000/05/10 19:34:44  drt
- * Further seperated system specific functions
- * and got didentd-name working again.
- *
- * Revision 1.7  2000/05/09 06:34:36  drt
- * Changed dataformat for IPv6 support
- *
- * Revision 1.6  2000/05/08 14:26:04  drt
- * IPv6 support, first try
- *
- * Revision 1.5  2000/04/30 02:01:58  drt
- * key is now taken from the enviroment
- *
- * Revision 1.4  2000/04/28 12:54:55  drt
- * Cleanup, better integration of libtai and dnscache
- *
- * Revision 1.3  2000/04/25 22:55:01  drt
- * code cleanups
- *
- * Revision 1.2  2000/04/19 13:40:27  drt
- * base64 a la MIME encoding - see RfC1341
- *
- * Revision 1.1.1.1  2000/04/12 16:07:19  drt
- * initial revision
  *
  */
 
@@ -54,7 +25,7 @@
 #include "rijndael.h"
 #include "txtparse.h"
 
-static char rcsid[] = "$Id: didentd-genanswer-crypt.c,v 1.9 2001/10/08 12:51:21 drt Exp $";
+static char rcsid[] = "$Id: didentd-genanswer-crypt.c,v 1.10 2001/10/11 15:43:07 drt Exp $";
 
 /* returns a pointer to a string describing a problem or NULL if
 sucessfull, adds to answer the part after the ports of an RfC 1413
@@ -64,7 +35,6 @@ char *generate_answer(stralloc *answer, uint32 uid,
 		      char *lip, uint16 lport, char *rip, uint16 rport)
 {
   char *x;
-  char *problem = "ok";
   char buf[5];
   stralloc out = {0};
   stralloc tmp = {0};
@@ -75,22 +45,23 @@ char *generate_answer(stralloc *answer, uint32 uid,
   if (!x)
       strerr_die2x(111, "didentd fatal: ", "$KEY not set");
 
+  /* initialize rijndael with $KEY */
   stralloc_copys(&key, x);
   txtparse(&key);
   pad(&key, 32);
-  
+  rijndaelKeySched(6, 8, key.s);
+    
+  /* build answer */
   stralloc_cats(answer, " : USERID : OTHER : ");
+
   uint32_pack(buf, uid); stralloc_catb(&tmp, buf, 4);
   uint16_pack(buf, lport); stralloc_catb(&tmp, buf, 2);
   uint16_pack(buf, rport); stralloc_catb(&tmp, buf, 2);
   uint32_pack(buf, time(NULL)); stralloc_catb(&tmp, buf, 4);
   stralloc_catb(&tmp, lip, 4);
   stralloc_catb(&tmp, rip, 8);
- 
-  /* initialize rijndael */
-  rijndaelKeySched(6, 8, key.s);
-  
-  /* encrypt with rijndael */
+
+  /* encrypt last part of answer with rijndael */
   rijndaelEncrypt(tmp.s);
   
   stralloc_readyplus(&out, 32);
@@ -100,7 +71,6 @@ char *generate_answer(stralloc *answer, uint32 uid,
   stralloc_cats(answer, "\r\n");
   stralloc_0(answer);
   
-  return problem;
+  return NULL;
 }
-
 
