7ecf8fbdf3e430c17d7851eb877330d37252d8b2
drt <drt@2d70b15c-62de-0310-b45c-a2b26695ab02>
2001-10-12 12:31:09 +0000

    pathnames fixed
    git-svn-id: http://svn.23.nu/svn/repos/didentd/trunk@313 2d70b15c-62de-0310-b45c-a2b26695ab02

--- didentd-test.sh
+++ didentd-test.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-# $Id: didentd-test.sh,v 1.1 2001/10/11 15:38:56 drt Exp $
+# $Id: didentd-test.sh,v 1.2 2001/10/12 12:31:09 drt Exp $
 # basic tests
 export KEY=test
+export ROOT=.
 export GID=0
 export PROTO=TCP
 export TCPLOCALIP=127.0.0.1
@@ -12,9 +13,9 @@ export TCPREMOTEPORT=8192
 tcpserver 127.0.0.1 4096 sh -c 'sleep 10'  &
 tcpclient -RHl0 -p 8192 -- 127.0.0.1 4096 sh -c 'exec cat <&6' &
 rm -f test.1 test.2 test.decrypt
-echo "8192 , 4096" | env UID=0 /home/drt/didentd/didentd 2> test.2 > test.crypt
-echo "8192 , 4096" | env UID=0 /home/drt/didentd/didentd-static 2>> test.2 >> test.1
-echo "8192 , 4096" | env UID=0 /home/drt/didentd/didentd-name 2>> test.2 >> test.1
+echo "8192 , 4096" | env UID=0 ./didentd 2> test.2 > test.crypt
+echo "8192 , 4096" | env UID=0 ./didentd-static 2>> test.2 >> test.1
+echo "8192 , 4096" | env UID=0 ./didentd-name 2>> test.2 >> test.1
 cat test.crypt | cut -c 32-63 | ./didentd-decrypt | cut -c 26-99 > test.decrypt
 # create files for compariastion
 USERID=`id -u`
@@ -23,6 +24,7 @@ rm -f test.should.1 test.should.2 test.should.decrypt
 echo -e "8192 , 4096 : USERID : UNIX : nope\015" > test.should.1
 echo -e "8192 , 4096 : USERID : UNIX : $USERNAME\015" >> test.should.1
 echo "127.0.0.1:8192 -> 127.0.0.1:4096 [ok] $USERID 8192 , 4096" > test.should.2
+echo "127.0.0.1:8192 -> 127.0.0.1:4096 [ok] $USERID 8192 , 4096" >> test.should.2
 echo "127.0.0.1:8192 -> 127.0.0.1:4096 [ok] $USERID 8192 , 4096" >> test.should.2
 echo "$USERNAME($USERID) 127.0.0.1:8192 127.0.0.1:4096" > test.should.decrypt
 if [ ! -z "`diff test.1 test.should.1`" ]; then
